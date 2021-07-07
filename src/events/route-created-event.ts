6a72d9f7285ad7ff5a075e67e450972eca1656c4
Hugo Cardoso <35729394+phoboPT@users.noreply.github.com>
2021-07-07 16:35:49 +0100

    update

--- src/events/route-created-event.ts
+++ src/events/route-created-event.ts
@@ -1,20 +1,22 @@
-import { Subjects } from "./subjects";
+import { Subjects } from './subjects';
 
 export interface RouteCreatedEvent {
-    subject: Subjects.RouteCreated
+    subject: Subjects.RouteCreated;
 
     data: {
-        id: string,
-        userId: string
-        type: string
-        startLocation: string
-        endLocation: string
-        availableTime: string
-        vehicleId: string
-        state: string
-        description: string
-        estimatedTime: string
-        startDate: string
-        userImage: string
-    }
+        id: string;
+        userId: string;
+        type: string;
+        startLocation: string;
+        endLocation: string;
+        availableTime: string;
+        vehicleId: string;
+        state: string;
+        description: string;
+        estimatedTime: string;
+        startDate: string;
+        userImage: string;
+        capacity: number;
+        actualCapacity: number;
+    };
 }
